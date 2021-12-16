52d811a9f686a10e8b371ac3277471d2387b5def
Ethan <56324629+500EJ@users.noreply.github.com>
2021-12-16 00:29:48 +0000

    Fetch daily weather and hourly weather URLs

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 let latitude = 40.7863;
 let longitude = -74.3301;
-let gridX;
-let gridY;
-let center;
 const POINTS_URL = `https://api.weather.gov/points/${latitude},${longitude}`;
+let GRIDPOINTS_DAY_URL;
+let GRIDPOINTS_HOURLY_URL
 
 const promise = fetch(POINTS_URL);
 promise
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (processedResponse) {
-        gridX = processedResponse.properties.gridX;
-        gridY = processedResponse.properties.gridY;
-        center = processedResponse.properties.cwa;
-    })
-
-const GRIDPOINTS_DAY_URL = `https://api.weather.gov/gridpoints/${center}/${gridX},${gridY}/forecast`;
-const GRIDPOINTS_HOURLY_URL = `https://api.weather.gov/gridpoints/${center}/${gridX},${gridY}/forecast/hourly`;
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (processedResponse) {
+    let gridX = processedResponse.properties.gridX;
+    let gridY = processedResponse.properties.gridY;
+    let center = processedResponse.properties.gridId;
+    GRIDPOINTS_DAY_URL = `https://api.weather.gov/gridpoints/${center}/${gridX},${gridY}/forecast`;
+    GRIDPOINTS_HOURLY_URL = `https://api.weather.gov/gridpoints/${center}/${gridX},${gridY}/forecast/hourly`;
+  })
